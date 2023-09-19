cbe992f6a3cc6b53a3703d156c77e70af71602f6
VanceNykreim <vanceyadi@msn.com>
2023-09-19 13:59:55 -0600

    Completed Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        int percent = int.Parse(Console.ReadLine());
+        string grade;
+        if (percent > 100 || percent < 0)
+        {
+            Console.Write("This is an invalid number.");
+            return;
+        }
+
+        if (percent >= 90)
+        {
+            grade = "A";
+        } else if (percent >= 80)
+        {
+            grade = "B";
+        } else if (percent >= 70)
+        {
+            grade = "C";
+        } else if (percent >= 60)
+        {
+            grade = "D";
+        } else 
+        {
+            grade = "F";
+        }
+        
+        if (percent % 10 >= 7 && percent >59)
+        {
+            grade = grade + "+";
+        } else if ( percent % 10 <= 3 && percent >59)
+        {
+            grade = grade + "-";
+        }
+
+        if (percent == 100)
+        {
+            grade = "A+";
+        }
+        Console.WriteLine($"Your grade is {grade}");
     }
 }
