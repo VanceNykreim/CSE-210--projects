c2b0327e7a3bb8d467061699162afa7b360179be
VanceNykreim <vanceyadi@msn.com>
2023-09-27 21:13:40 -0600

    finished prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
-using System;
+using System.Collections.Generic;
 
-class Program
+Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+// create the list
+List<decimal> numbers = new List<decimal>();
+decimal number = -1;
+
+//fills the list
+while (number != 0)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep4 World!");
+    Console.Write("Enter number: ");
+    number = decimal.Parse(Console.ReadLine());
+    if (number != 0){
+        numbers.Add(number);
     }
 }
+
+//find the sum of the contents of the list
+decimal sum = 0;
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    sum += numbers[i];
+}
+
+//find average of the list
+decimal avg = sum/numbers.Count; 
+
+//find the largest number in the list
+decimal biggest = 0;
+
+for (int i = 0; i < numbers.Count; i++)
+{
+    if (biggest < numbers[i]) {
+        biggest = numbers[i];
+    }
+}
+
+Console.WriteLine("The sum is: " + sum);
+Console.WriteLine("The average is: " + avg);
+Console.WriteLine("The largest number is: " + biggest);
