67b47174e1b1495e4736326101806510000db9c9
VanceNykreim <vanceyadi@msn.com>
2023-09-27 20:13:04 -0600

    finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -2,8 +2,25 @@ using System;
 
 class Program
 {
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicnum = int.Parse(Console.ReadLine());
+
+        int guess = 0;
+        do
+        {Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+
+        if (magicnum > guess) {
+            Console.WriteLine("Higher");
+        } else if (magicnum < guess) {
+            Console.WriteLine("Lower");
+        } 
+        } while (magicnum != guess);
+
+        Console.Write("You guessed it!");
+        
     }
 }
