5f5143ab902f0e1ab7aa171a2c1630bf0cadaed6
VanceNykreim <vanceyadi@msn.com>
2023-09-27 21:31:01 -0600

    finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Data.SqlTypes;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int favnum = PromptUserNumber();
+        int squarenum = SquareNumber(favnum);
+        DisplayResult(name, squarenum);
+    }
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favnum = int.Parse(Console.ReadLine());
+        return favnum;
+    }
+
+    static int SquareNumber(int favnum)
+    {
+        return favnum * favnum;
+    }
+
+    static void DisplayResult(string username, int squarenum)
+    {
+        Console.WriteLine(username + ", the square of your number is " + squarenum);
     }
 }
