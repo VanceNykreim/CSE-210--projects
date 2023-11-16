d268fc9c28e9a10597cd868ac4637b22475ee57c
VanceNykreim <vanceyadi@msn.com>
2023-11-16 14:15:22 -0700

    Finished W5 prepare

--- prepare/Learning03/Fraction.cs
+++ prepare/Learning03/Fraction.cs
@@ -21,5 +21,33 @@ public class Fraction
         _bottom = bottom;
     }
 
-    
+    public int GetTop() 
+    {
+        return _top;
+    }
+
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public int GetBottom() 
+    {
+        return _bottom;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
 }
--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+
+        string frac1string = fraction1.GetFractionString();
+
+        Console.WriteLine(frac1string);
+
+        double frac1double = fraction1.GetDecimalValue();
+
+        Console.WriteLine(frac1double.ToString("0.#"));
+
+        Fraction fraction2 = new Fraction(5);
+
+        string frac2string = fraction2.GetFractionString();
+
+        Console.WriteLine(frac2string);
+
+        double frac2double = fraction2.GetDecimalValue();
+
+        Console.WriteLine(frac2double.ToString("0.#"));
+
+        Fraction fraction3 = new Fraction(3, 4);
+
+        string frac3string = fraction3.GetFractionString();
+
+        Console.WriteLine(frac3string);
+
+        double frac3double = fraction3.GetDecimalValue();
+
+        Console.WriteLine(frac3double.ToString("F2"));
     }
 }
